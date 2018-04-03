99e38fd9bca835d6ff6438e0f874fe67dcbd7dd4
Marko Tagliavia <max.tagliavia@gmail.com>
2018-04-03 13:50:11 +0200

    Login UI dodat i u Administraciju i Racunovodstvo. Popravljene animacije za menjanje boja

--- Skladistenje/LoginWindow.xaml.cs
+++ Skladistenje/LoginWindow.xaml.cs
@@ -21,12 +21,16 @@ namespace Skladistenje
     /// </summary>
     public partial class LoginWindow : Window, INotifyPropertyChanged
     {
+        #region Fields
         private System.Windows.Media.Color c1;
         private System.Windows.Media.Brush _firmColor;
         private System.Windows.Media.Color c2;
         private System.Windows.Media.Brush _lightFirmColor;
-
-        
+        private System.Windows.Media.Color c3;
+        private System.Windows.Media.Brush _backgroundColor;
+        private System.Windows.Media.Brush _buttonColor;
+        private System.Windows.Media.Brush _labelColor;
+        #endregion
 
         #region INotifiedProperty Block
         protected void OnPropertyChanged(string porpName)
@@ -42,8 +46,13 @@ namespace Skladistenje
         public LoginWindow()
         {
             c1 = System.Windows.Media.Color.FromArgb(255, 68, 95, 245);
-            c2 = System.Windows.Media.Color.FromArgb(255, 100, 105, 245);
+            c2 = System.Windows.Media.Color.FromArgb(255, 128, 170, 255);
+            c3 = System.Windows.Media.Color.FromArgb(255, 204, 179, 255);
             FirmColor = new SolidColorBrush(c1);
+            LightFirmColor = new SolidColorBrush(c2);
+            BackgroundColor = new SolidColorBrush(c3);
+            ButtonColor = new SolidColorBrush(c1);
+            LabelColor = new SolidColorBrush(c1);
             InitializeComponent();
             this.DataContext = this;
         }
@@ -66,11 +75,42 @@ namespace Skladistenje
                 OnPropertyChanged("LightFirmColor");
             }
         }
+
+        public Brush BackgroundColor
+        {
+            get { return _backgroundColor; }
+            set
+            {
+                _backgroundColor = value;
+                OnPropertyChanged("BackgroundColor");
+            }
+        }
+
+        public Brush ButtonColor
+        {
+            get { return _buttonColor; }
+            set
+            {
+                _buttonColor = value;
+                OnPropertyChanged("ButtonColor");
+            }
+        }
+
+        public Brush LabelColor
+        {
+            get { return _labelColor; }
+            set
+            {
+                _labelColor = value;
+                OnPropertyChanged("LabelColor");
+            }
+        }
         #endregion
 
-
+        #region EventsColors
         private void usernameTextBoxFocus(object sender, RoutedEventArgs e)
         {
+            if (usernameTextBox.Text.Equals("Korisnicko ime")) usernameTextBox.Text = "";
             Storyboard sb = new Storyboard() { Duration = TimeSpan.FromSeconds(1), BeginTime = TimeSpan.Zero };
 
             ColorAnimation colAnim = new ColorAnimation();
@@ -103,22 +143,62 @@ namespace Skladistenje
 
         private void passwordBoxFocus(object sender, RoutedEventArgs e)
         {
+            Storyboard sb = new Storyboard() { Duration = TimeSpan.FromSeconds(1), BeginTime = TimeSpan.Zero };
 
+            ColorAnimation colAnim = new ColorAnimation();
+            colAnim.To = c1;
+            colAnim.Duration = new Duration(TimeSpan.FromSeconds(1));
+            colAnim.AutoReverse = false;
+
+            sb.Children.Add(colAnim);
+
+            Storyboard.SetTarget(colAnim, kvadrat2);
+            Storyboard.SetTargetProperty(colAnim, new PropertyPath("Fill.Color"));
+            sb.Begin();
         }
 
         private void passwordBoxUnFocus(object sender, RoutedEventArgs e)
         {
+            Storyboard sb = new Storyboard() { Duration = TimeSpan.FromSeconds(1), BeginTime = TimeSpan.Zero };
 
+            ColorAnimation colAnim = new ColorAnimation();
+            colAnim.To = System.Windows.Media.Color.FromArgb(255, 177, 177, 177);
+            colAnim.Duration = new Duration(TimeSpan.FromSeconds(1));
+            colAnim.AutoReverse = false;
+
+            sb.Children.Add(colAnim);
+
+            Storyboard.SetTarget(colAnim, kvadrat2);
+            Storyboard.SetTargetProperty(colAnim, new PropertyPath("Fill.Color"));
+            sb.Begin();
         }
 
         private void prijavaMouseEnter(object sender, MouseEventArgs e)
         {
-
+            ButtonColor = LightFirmColor;
         }
 
         private void prijavaMouseLeave(object sender, MouseEventArgs e)
         {
-
+            ButtonColor = FirmColor;
         }
+
+        private void labelEnter(object sender, MouseEventArgs e)
+        {
+            LabelColor = LightFirmColor;
+        }
+
+        private void labelLeave(object sender, MouseEventArgs e)
+        {
+            LabelColor = FirmColor;
+        }
+        #endregion
+
+        #region EventsLogic
+        private void prijaviSe(object sender, RoutedEventArgs e)
+        {
+            //TO DO: prijava
+        }
+        #endregion EventsLogic
     }
 }
